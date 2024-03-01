25c90f29d4a815b5b6d7c37acfa7e2769425f60e
Lixen09 <161814991+Lixen09@users.noreply.github.com>
2024-03-01 09:34:57 +0100

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    for(int i=15; i; i--)
+    {
+        cout << i << endl;
+    }
+    cout << "JEBUT";
+    return 0 ; 
+} 
